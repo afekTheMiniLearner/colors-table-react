f1677a1fa7b677e8805d43eae7d1ce1bbed650ea
afekTheMiniLearner <afeksa2000@gmail.com>
2023-01-28 14:07:49 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import { matrix } from "./utils/consts";
 import { Square } from "./base-components";
+import { generateRandomColor } from "./utils/functions";
 
 function App() {
-  console.log(JSON.stringify(matrix));
+  const [colorState, setColorState] = useState(generateRandomColor());
 
   return (
     <div className="row">
       {matrix.map((row, i) => {
         return row.map((color, j) => {
-          
-          return <Square value={colorState} key={`${i}-${j}`} />;
+          return (
+            <Square
+              value={colorState}
+              setValue={setColorState}
+              key={`${i}-${j}`}
+            />
+          );
         });
       })}
     </div>
--- src/base-components/Sqaure/Square.jsx
+++ src/base-components/Sqaure/Square.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export function Square({ value }) {
-  console.log(value);
+export function Square({ value, setValue }) {
   return (
-    <div className="square" style={{ backgroundColor: value }}>
-    </div>
+    <div
+      className="square"
+      style={{ backgroundColor: value }}
+      onClick={setValue()}
+    ></div>
   );
 }
+
+Square.propTypes = {
+  value: PropTypes.string,
+  setValue: PropTypes.func,
+};
+
+Square.defaultProps = {};

fc2e9ed5388f79cdf9d48f5590d95a2b3c719068
afekTheMiniLearner <afeksa2000@gmail.com>
2023-03-05 16:12:10 +0200

    updating the export of the component for NPM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ManagedColorsTable } from "./components";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
@@ -11,3 +12,5 @@ root.render(<App />);
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export { ManagedColorsTable };
